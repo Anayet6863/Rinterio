54bcea94b5ddc138a6f5e70959e83e25b2519e72
anayet6866 <mdsaadahmed6866@gmail.com>
2024-08-22 15:53:35 +0600

    Complete nav and banner section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundcolor:{
+        'primary-1': '#FFFFFF'
+      },
     },
     plugins: [],
   }
+}
